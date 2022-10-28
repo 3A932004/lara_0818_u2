8d825e06219543316e51e495ca1c88770150d78d
lai yu tien <s3a932004@student.ncut.edu.tw>
2022-10-29 00:20:38 +0800

    傳遞資料至View顯示

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ Route::get('/', function () {
 //    return view('home.index');
 //});
 Route::get('home', [HomeController::class, 'index']);
+Route::get('hello/{name?}', [HelloController::class, 'index'])->name('hello.index');
